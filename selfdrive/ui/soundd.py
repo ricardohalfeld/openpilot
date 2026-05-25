d28c598ec19abd2147cb756521a3d79bbf5b0708
Ricardo Halfeld <ricardo.halfeld@gmail.com>
2026-05-25 11:24:57 -0300

    Use standard engage sounds on Creta

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -45,12 +45,6 @@ sound_list: dict[int, tuple[str, int | None, float]] = {
   AudibleAlert.warningSoft: ("warning_soft.wav", None, MAX_VOLUME),
   AudibleAlert.warningImmediate: ("warning_immediate.wav", None, MAX_VOLUME),
 }
-if HARDWARE.get_device_type() == "tizi":
-  sound_list.update({
-    AudibleAlert.engage: ("engage_tizi.wav", 1, MAX_VOLUME),
-    AudibleAlert.disengage: ("disengage_tizi.wav", 1, MAX_VOLUME),
-  })
-
 def check_selfdrive_timeout_alert(sm):
   ss_missing = time.monotonic() - sm.recv_time['selfdriveState']
 
